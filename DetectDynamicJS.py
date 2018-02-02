e017b4998931401cd5dccf689e644713377e9c81
Paul Johnston <paul.johnston@portswigger.net>
2018-02-02 16:25:55 +0000

    Bump version

--- DetectDynamicJS.py
+++ DetectDynamicJS.py
@@ -30,7 +30,7 @@ try:
 except ImportError:
     print "Failed to load dependencies. This issue maybe caused by using an unstable Jython version."
 
-VERSION = '0.8'
+VERSION = '0.9'
 VERSIONNAME = 'Honey Bunny'
 
 
